596528817c99398a682f6dae19510a40a0d82751
LUAN228 <luanfghsilverio@gmail.com>
2026-09-05 21:51:45 -0300

    Modify JitPack repository settings and project name
    Updated repository URL configuration and project name.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,12 +7,18 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url = java.net.URI("https://jitpack.io") }
+        maven {
+            url = uri("https://jitpack.io")
+            credentials {
+                username = System.getenv("JITPACK_TOKEN") ?: ""
+            }
+        }
     }
 }
 
-rootProject.name = "PizzaOvenMobile"
+rootProject.name = "Pizza Oven Plus Mobile"
 include(":app")

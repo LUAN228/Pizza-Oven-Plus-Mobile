c18f7825b8cc505b3e1116649fbf0ab42f3c4fef
LUAN228 <luanfghsilverio@gmail.com>
2026-09-05 21:41:11 -0300

    Update repositoriesMode and add JitPack repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,10 +6,11 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOSITORIES)
+    repositoriesMode.set(org.gradle.api.initialization.resolve.RepositoriesMode.FAIL_ON_PROJECT_REPOSITORIES)
     repositories {
         google()
         mavenCentral()
+        maven { url = java.net.URI("https://jitpack.io") }
     }
 }
 rootProject.name = "PizzaOvenMobile"
